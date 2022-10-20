2a6bfb527fd5277b8e68073214747caa9eb2d9f2
Mohamed Abohassan <mohamed.ah.abohassan@gmail.com>
2022-10-20 14:57:21 +0200

    twitter_airfow

---
+++ twitter_dag.py
@@ -0,0 +1,33 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+from twitter_etl import run_twitter_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 10, 20),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'twitter_dag',
+    default_args=default_args,
+    description='My DAG !',
+    # schedule_interval=timedelta(days=1),
+)
+
+
+run_etl = PythonOperator(
+    task_id='complete_twitter_etl',
+    python_callable=run_twitter_etl,
+    dag=dag, 
+)
+
+run_etl
